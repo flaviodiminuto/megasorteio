e19e462e499a0b721a30a0933b1b031f2025301c
Flávio <flavio.diminuto@outlook.com>
2019-02-17 14:21:21 -0300

    Concluída classe Sequencia e seus testes

--- app/src/test/java/com/flavio/android/megasorteio/SequenciaTest.kt
+++ app/src/test/java/com/flavio/android/megasorteio/SequenciaTest.kt
@@ -6,11 +6,45 @@ import org.junit.jupiter.api.RepeatedTest
 import org.junit.jupiter.api.Test
 
 class SequenciaTest {
-    var sequencia = Sequencia(6)
+    var sequencia = Sequencia(4)
 
-    @RepeatedTest(5000)
-    fun testGeraNumero(){
-        assertTrue(sequencia.geraNumero() >= 1 && sequencia.geraNumero() <= 60)
+    @RepeatedTest(10)
+    fun testGerarSequencia(){
+        assertNotEquals(sequencia.gerarSequencia(),sequencia.gerarSequencia())
+    }
+    @Test
+    fun testTamanhoSequencia(){
+        val sequencia1 = Sequencia(4)
+        assertEquals(5,sequencia1.tamanho)
+
+        val sequencia2 = Sequencia(7)
+        assertEquals(7,sequencia2.tamanho)
+
+        val sequencia3 = Sequencia(15)
+        assertEquals(14,sequencia3.tamanho)
 
     }
+
+    @Test
+    fun testOrdenaNumeroSequencai(){
+        val sequenciaDesordenada = arrayListOf<Int>(6,5,4,3,2,1)
+        assertEquals(sequencia.ordenaNumerosSequencia(sequenciaDesordenada), arrayListOf<Int>(1,2,3,4,5,6))
+
+    }
+    @RepeatedTest(10)
+    fun testaSequenciaUnica(){
+        val quantidadeSequencias = 3
+        var sequenciaInicial = arrayListOf<Sequencia>()
+        for(i in 0..quantidadeSequencias) {
+            sequenciaInicial.add(Sequencia(6))
+        }
+        assertNotEquals(sequenciaInicial.toString(),sequencia.sequenciaUnica(sequenciaInicial).toString())
+    }
+
+    @Test
+    fun testaToString(){
+        var sequencia = Sequencia(arrayListOf(1,2,3,4,5,6))
+        assertEquals("1 2 3 4 5 6", sequencia.toString())
+    }
+
 }
