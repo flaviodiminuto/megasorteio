2d129bf32eb107c7dc66e0f05b9a7c30a8759c03
Flávio da Sílva Santos <flavio.diminuto@outlook.com>
2019-05-31 22:35:15 -0300

    Criacao tabela aposta_sequencia (Finalizada)

--- app/src/main/java/com/flavio/android/megasorteio/database/Banco.kt
+++ app/src/main/java/com/flavio/android/megasorteio/database/Banco.kt
@@ -30,31 +30,33 @@ class Banco(context: Context) : SQLiteOpenHelper(context, db_name,null,version) 
         val sql = "CREATE TABLE IF NOT EXISTS sequencia " +
                 "(${Campos.SEQUENCIA_ID.nome} INTEGER PRIMARY KEY AUTOINCREMENT," +
                 " ${Campos.SEQUENCIA_VALOR.nome} REAL  DEFAULT 0," +
-                " data_cadastro TEXT ," +
-                " data_atualizacao TEXT, " +
-                " n1 INTEGER DEFAULT 0, " +
-                " n2 INTEGER DEFAULT 0, " +
-                " n3 INTEGER DEFAULT 0, " +
-                " n4 INTEGER DEFAULT 0, " +
-                " n5 INTEGER DEFAULT 0, " +
-                " n6 INTEGER DEFAULT 0, " +
-                " n7 INTEGER DEFAULT 0, " +
-                " n8 INTEGER DEFAULT 0, " +
-                " n9 INTEGER DEFAULT 0, " +
-                " n10 INTEGER DEFAULT 0, " +
-                " n11 INTEGER DEFAULT 0, " +
-                " n12 INTEGER DEFAULT 0, " +
-                " n13 INTEGER DEFAULT 0, " +
-                " n14 INTEGER DEFAULT 0, " +
-                " n15 INTEGER DEFAULT 0 ) "
+                " ${Campos.SEQUENCIA_DATA_CADASTRO.nome} TEXT ," +
+                " ${Campos.SEQUENCIA_DATA_ATUALIZACAO.nome} TEXT, " +
+                " ${Campos.SEQUENCIA_N1.nome} INTEGER , " +
+                " ${Campos.SEQUENCIA_N2.nome} INTEGER , " +
+                " ${Campos.SEQUENCIA_N3.nome} INTEGER , " +
+                " ${Campos.SEQUENCIA_N4.nome} INTEGER , " +
+                " ${Campos.SEQUENCIA_N5.nome} INTEGER , " +
+                " ${Campos.SEQUENCIA_N6.nome} INTEGER , " +
+                " ${Campos.SEQUENCIA_N7.nome} INTEGER , " +
+                " ${Campos.SEQUENCIA_N8.nome} INTEGER , " +
+                " ${Campos.SEQUENCIA_N9.nome} INTEGER , " +
+                " ${Campos.SEQUENCIA_N10.nome} INTEGER , " +
+                " ${Campos.SEQUENCIA_N11.nome} INTEGER , " +
+                " ${Campos.SEQUENCIA_N12.nome} INTEGER , " +
+                " ${Campos.SEQUENCIA_N13.nome} INTEGER , " +
+                " ${Campos.SEQUENCIA_N14.nome} INTEGER , " +
+                " ${Campos.SEQUENCIA_N15.nome} INTEGER ) "
         db?.execSQL(sql)
     }
 
     private fun createApostaSequenciaTable(db: SQLiteDatabase?){
         val sql = "CREATE TABLE IF NOT EXISTS ${Campos.APOSTA_SEQUENCIA_TABLE.nome} " +
-                " ( ${Campos.APOSTA_SEQUENCIA_ID} INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                " ${Campos.APOSTA_SEQUENCIA_APOSTA} INTEGER, " +
-                " ${Campos.APOSTA_SEQUENCIA_SEQUENCIA} INTEGER)"
+                " ( ${Campos.APOSTA_SEQUENCIA_ID.nome} INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                " ${Campos.APOSTA_SEQUENCIA_APOSTA.nome} INTEGER, " +
+                " ${Campos.APOSTA_SEQUENCIA_SEQUENCIA.nome} INTEGER, " +
+                " FOREIGN KEY(${Campos.APOSTA_SEQUENCIA_APOSTA.nome}) REFERENCES ${Campos.APOSTA_TABLE.nome}(${Campos.APOSTA_ID.nome}) ON DELETE CASCADE ON UPDATE CASCADE," +
+                " FOREIGN KEY(${Campos.APOSTA_SEQUENCIA_SEQUENCIA.nome}) REFERENCES ${Campos.SEQUENCIA_TABLE.nome}(${Campos.SEQUENCIA_ID.nome}) ON DELETE CASCADE ON UPDATE CASCADE )"
         db?.execSQL(sql)
     }
 
